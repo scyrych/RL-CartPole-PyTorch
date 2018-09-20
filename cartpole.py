1a16765408bf58c04526d4b3b631890147489526
zoskia <zoskia@o2.pl>
2018-09-20 05:56:55 +0200

    working example

--- cartpole.py
+++ cartpole.py
@@ -1,46 +1,50 @@
 import gym
-from collections import namedtuple
-import numpy as np
 from tensorboardX import SummaryWriter
-
-import torch
+from net_model import Net
 import torch.nn as nn
 import torch.optim as optim
-
-from model import Net
+from batch_helpers import iterate_batches, filter_batch
 
 HIDDEN_SIZE = 128
 BATCH_SIZE = 16
 PERCENTILE = 70
 
-Episode = namedtuple('Episode', field_names=['reward', 'steps'])
-EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
+
+def main():
+    env = gym.make("CartPole-v0")
+    obs_size = env.observation_space.shape[0]
+    env = gym.wrappers.Monitor(env, directory="mon", force=True)
+    n_actions = env.action_space.n
+
+    net = Net(obs_size, HIDDEN_SIZE, n_actions)
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.01)
+    writer = SummaryWriter(comment="-cartpole")
+
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        optimizer.zero_grad()
+        action_scores_v = net(obs_v)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+
+        create_tensorboard_report(iter_no, loss_v, reward_b, reward_m, writer)
+
+        if reward_m > 199:
+            print("Solved!")
+            break
+
+    writer.close()
 
 
-def iterate_batches(env, net, batch_size):
-    batch = []
-    episode_reward = 0.0
-    episode_steps = []
-    obs = env.reset()
-    sm = nn.Softmax(dim=1)
+def create_tensorboard_report(iter_no, loss_v, reward_b, reward_m, writer):
+    print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (
+        iter_no, loss_v.item(), reward_m, reward_b))
+    writer.add_scalar("loss", loss_v.item(), iter_no)
+    writer.add_scalar("reward_bound", reward_b, iter_no)
+    writer.add_scalar("reward_mean", reward_m, iter_no)
 
-    while True:
-        obs_v = torch.FloatTensor([obs])
-        act_probs_v = sm(net(obs_v))
-        act_probs = act_probs_v.data.numpy()[0]
-        action = np.random.choice(len(act_probs), p=act_probs)
-        next_obs, reward, is_done, _ = env.step(action)
-        episode_reward += reward
-        episode_steps.append(EpisodeStep(observation=obs, action=action))
 
-        if is_done:
-            batch.append(Episode(reward=episode_reward, steps=episode_steps))
-            episode_reward = 0.0
-            episode_steps = []
-            next_obs = env.reset()
-
-            if len(batch) == batch_size:
-                yield batch
-                batch = []
-
-        obs = next_obs
+if __name__ == '__main__':
+    main()
